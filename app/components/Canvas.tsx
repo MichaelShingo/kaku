4017eabc4daa8439d0a98214918d485b43f7c548
MichaelShingo <mcrawford5376@gmail.com>
2024-04-28 11:11:31 +0200

    p5 wrapper setup

--- app/components/Canvas.tsx
+++ app/components/Canvas.tsx
@@ -1,33 +1,25 @@
 'use client';
-import { useEffect, useRef } from 'react';
-import p5 from 'p5';
 
-const x = 35;
-let p: p5 | null = null;
+import { type Sketch } from '@p5-wrapper/react';
+import { NextReactP5Wrapper } from '@p5-wrapper/next';
+
+const sketch: Sketch = (p5) => {
+	p5.setup = () => p5.createCanvas(600, 400, p5.WEBGL);
+
+	p5.draw = () => {
+		p5.background(250);
+		p5.normalMaterial();
+		p5.push();
+		p5.rotateZ(p5.frameCount * 0.01);
+		p5.rotateX(p5.frameCount * 0.01);
+		p5.rotateY(p5.frameCount * 0.01);
+		p5.plane(100);
+		p5.pop();
+	};
+};
 
 const Canvas = () => {
-	const canvasRef = useRef<HTMLDivElement>(null);
-	useEffect(() => {
-		if (canvasRef.current) {
-			p = new p5((p: p5) => {
-				p.setup = () => {
-					p.createCanvas(700, 500).parent(canvasRef.current!);
-				};
-
-				p.draw = () => {
-					p!.background(255, 220, 20);
-					if (p!.mouseIsPressed) {
-						p!.fill(0);
-					} else {
-						p!.fill(255);
-					}
-					p!.ellipse(p!.mouseX, p!.mouseY, 80, 80);
-				};
-			});
-		}
-	}, []);
-
-	return <div className="border-3 border-black" ref={canvasRef}></div>;
+	return <NextReactP5Wrapper sketch={sketch} />;
 };
 
 export default Canvas;
